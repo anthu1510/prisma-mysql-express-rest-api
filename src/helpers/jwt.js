8174394cdaa06804500bd7e862f618c3e803906b
anthu1510 <anthu1510@gmail.com>
2022-07-08 20:53:46 +0530

    Reformat helpers/jwt.js file

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -1,38 +1,39 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = {
-  generateAccessToken: (payload) => {
-    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: process.env.EXPIRE_ACCESS_TOKEN_TIME,
-    });
-  },
-  generateRefreshToken: (payload) => {
-    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
-      expiresIn: process.env.EXPIRE_REFRESH_TOKEN_TIME,
-    });
-  },
-  verifyToken: (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+exports.generateAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.EXPIRE_ACCESS_TOKEN_TIME,
+  });
+};
 
-    if (token == null) return res.sendStatus(401);
+exports.generateRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.EXPIRE_REFRESH_TOKEN_TIME,
+  });
+};
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
-      req.user = user;
-      next();
-    });
-  },
-  newTokens: (refreshtoken) => {
-    let result = null;
+exports.verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!refreshtoken) result = false;
+  if (token == null) return res.sendStatus(401);
 
-    jwt.verify(refreshtoken, process.env.REFRESH_TOKEN_SECRET, (err, rs) => {
-      if (err) throw err;
-      result = rs;
-    });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
-    return result;
-  },
+exports.newTokens = (refreshtoken) => {
+  let result = null;
+
+  if (!refreshtoken) result = false;
+
+  jwt.verify(refreshtoken, process.env.REFRESH_TOKEN_SECRET, (err, rs) => {
+    if (err) throw err;
+    result = rs;
+  });
+
+  return result;
 };
